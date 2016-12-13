3f540d6489676fad4b17cf183df4bf76898c04b7
fnsne <watashiwafnsne@gmail.com>
2016-12-13 21:40:08 +0800

    commit一些東西 在上一個commit裡面少commit的

--- public/javascripts/course_card.js
+++ public/javascripts/course_card.js
@@ -1,7 +1,7 @@
 /**
  * Created by fnsne on 2016/12/7.
  */
-var json = '{ "courses" :[ {"name": "計算機圖學", "class_number": "CS314", "class": "A", "credit" : "3", "type" : "選修", "teacher": "葉亦成", "attr" : "學程A", "position" : "1201A", "difficulty" : "3", "rating": "3.8", "tag" : ""},{"name": "程式設計(一)", "class_number": "CS301", "class": "A", "credit" : "3", "type" : "必修", "teacher": "林基成", "attr" : "學程B", "position" : "1401B", "difficulty" : "4", "rating": "4.7", "tag" : ""}]}';
+var json = '{ "courses" :[ {"name": "計算機圖學", "class_number": "CS314", "class": "A", "credit" : "3", "type" : "選修", "teacher": "葉亦成", "attr" : "學程A", "position" : "1201A", "difficulty" : "3", "rating": "3.8", "tag" : ["tag1, tag2"] },{"name": "程式設計(一)", "class_number": "CS301", "class": "A", "credit" : "3", "type" : "必修", "teacher": "林基成", "attr" : "學程B", "position" : "1401B", "difficulty" : "4", "rating": "4.7", "tag" : ["tag3, tag4, tag5"]}]}';
 var obj = JSON.parse(json);
 var star_image = "/assets/stars.png";
 var id_num = 0;
@@ -20,10 +20,16 @@ document.addEventListener('DOMContentLoaded', function(){
         for(var i = 0 ; i < 2; i++){
             var course_card = document.querySelector('#course_card_template');
             course_card.content.querySelector('.course_name').innerText = obj.courses[i].name;
-            course_card.content.querySelector('.couse_teacher').innerText = obj.courses[i].teacher;
 
-            insert_course_name(course_card.content, obj.courses[i].class_number, obj.courses[i].class, obj.courses[i].credit);
-            course_card.content.querySelector('.rating').innerText = obj.courses[i].rating;
+            insert_course_name(course_card.content, obj.courses[i].name);
+            insert_course_class(course_card.content, obj.courses[i].class_number, obj.courses[i].class, obj.courses[i].credit);
+            insert_course_type(course_card.content, obj.courses[i].type);
+            insert_course_teacher(course_card.content, obj.courses[i].teacher);
+            insert_course_attribution(course_card.content, obj.courses[i].attr);
+            insert_course_position(course_card.content, obj.courses[i].position);
+            insert_course_rating(course_card.content, obj.courses[i].rating);
+            insert_course_difficulty(course_card.content, obj.courses[i].difficulty);
+
             var clone = document.importNode(course_card.content, true);
             courses.appendChild(clone);
         }
@@ -33,10 +39,38 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 });
 
-function insert_course_name( course_content, course_number, course_class, course_credit ) {
+function insert_course_name( course_content, name ) {
+    course_content.querySelector('.course_name').innerText = name;
+}
+
+function insert_course_class( course_content, course_number, course_class, course_credit ) {
     course_content.querySelector('.class_number').innerText = course_number;
     course_content.querySelector('.class').innerText = course_class+"班";
     course_content.querySelector('.credit').innerText = course_credit+"學分";
+}
+
+function insert_course_type( course_content, course_type ) {
+    course_content.querySelector('.type').innerText = course_type;
+}
+
+function insert_course_teacher( course_content, teacher_name ) {
+    course_content.querySelector('.couse_teacher').innerText = teacher_name;
+}
+
+function insert_course_attribution( course_content, attr ) {
+    course_content.querySelector('.attr').innerText = attr;
+}
+
+function insert_course_position( course_content, position ) {
+    course_content.querySelector('.position').innerText = "地點 : "+position;
+}
+
+function insert_course_rating( course_content, rating ) {
+    course_content.querySelector('.difficulty').innerText = "評分: "+rating;
+}
+
+function insert_course_difficulty( course_content, difficulty ) {
+    course_content.querySelector('.difficulty').innerText = "難度 : "+difficulty;
 }
 
 function supportsTemplate() {

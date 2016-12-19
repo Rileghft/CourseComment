2a875f675a3804dfc5846ff9e7df08620859ac72
楊舜宇 <mywonderemailyoung@gmail.com>
2016-12-19 23:26:25 +0800

    回復報刪除檔案，修正學年計算

---
+++ app/controllers/CourseCardQuery.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.bson.Document;
+import org.jongo.MongoCollection;
+import org.jongo.MongoCursor;
+import play.mvc.BodyParser;
+import play.mvc.Controller;
+import play.mvc.Result;
+import mongo.MongoClientFactory;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ * Created by 楊舜宇 on 2016/12/18.
+ */
+public class CourseCardQuery extends Controller{
+
+    public Result defaultCards() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int roc_year = year - 1911;
+        int semester = (month > 11 || month < 5)? 2: 1;
+        int semester_year = roc_year - (semester == 2? 1: 0);
+        MongoCollection cardDB = MongoClientFactory.getCollection(String.format("s%d%d.courseCard", semester_year, semester));
+        MongoCursor<Document> cards = cardDB.find("{}").limit(20).as(Document.class);
+        ArrayList<String> results = new ArrayList<>(cards.count());
+        cards.forEach(card -> {
+            card.remove("_id");
+            results.add(card.toJson());
+        });
+        return ok(results.toString()).as("application/json");
+    }
+
+    @BodyParser.Of(BodyParser.Json.class)
+    public Result moreCards() {
+        JsonNode option = request().body().asJson();
+        return badRequest("not implement.");
+    }
+}

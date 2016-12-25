b9b05b0ce309cc91eadc397d01eec1716db6a060
fnsne <watashiwafnsne@gmail.com>
2016-12-25 19:45:26 +0800

    修改卡片使其可以使用新格式的資料 但是舜宇資料怪怪的

--- public/javascripts/course_card.js
+++ public/javascripts/course_card.js
@@ -36,7 +36,7 @@ function getCourses(courses) {
             insert_course_type(course_card.content, obj[i].type);
             insert_course_teachers(course_card.content, obj[i].teachers);
             insert_course_attribution(course_card.content, obj[i].attrs);
-            insert_course_place_time(course_card.content, obj[i].place_time);
+            insert_course_times( course_card.content, obj[i].times);
             insert_course_rating(course_card.content, obj[i].recommend);
             insert_course_difficulty(course_card.content, obj[i].difficulty);
             //insert_course_tags(course_card.content, obj[i].tags);
@@ -90,10 +90,12 @@ function insert_course_attribution( course_content, attrs ) {
     }
 }
 
-function insert_course_place_time( course_content, place_time ) {
-    var place = Object.keys(place_time);
-    var time = place_time[place];
-    course_content.querySelector('.position').innerText = "地點 : " + place + ", 時間 : " +  time;
+function insert_course_times( course_content, times ) {
+    var timeStr = "時間 : " + times[0];
+    for(var i = 1 ; i < times.length; i++){
+        timeStr += ", " +times[i];
+    }
+    course_content.querySelector('.time').innerText = timeStr;
 }
 
 function insert_course_rating( course_content, rating ) {

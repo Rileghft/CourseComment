4c1e4fdb849c389272ee1be6e97b58934af9d1a4
fnsne <watashiwafnsne@gmail.com>
2016-12-25 16:19:12 +0800

    修改search filter的設計

--- public/javascripts/filter.js
+++ public/javascripts/filter.js
@@ -6,11 +6,12 @@ document.addEventListener('DOMContentLoaded', function(){
     $('.filter_element').click( function () {
         var $box = $(this);
         if($box.is(":checked")) {
-            var group = "input:radio[name='" + $box.attr("name") + "']";
+            /*var group = "input:radio[name='" + $box.attr("name") + "']";
             $(group).prop("checked", false);
-            $box.prop("checked", true);
+            $box.prop("checked", true);*/
+            //在這邊做點擊後要做的反應
         }else{
-            $box.prop("checked",false);
+           /* $box.prop("checked",false);*/
         }
     })
 

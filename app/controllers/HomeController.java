97574965ddfbb6f1b0c764e231cc6f527b8dc0ad
DESKTOP-PVTHLK3\Jomini_Lin <vince2641@gmail.com>
2016-12-31 16:50:37 +0800

    Add course information page.

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -21,4 +21,6 @@ public class HomeController extends Controller {
     }
 
     public Result showStudentQuestionnaire() { return ok(student_questionnaire_mainPage.render("學生評論")); }
+
+    public Result showCourseInformation() { return ok(course_information_mainPage.render("課程資訊")); }
 }

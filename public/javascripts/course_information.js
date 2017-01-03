765ed555c3c26f6803eeb83c097681b0145a6ffc
DESKTOP-PVTHLK3\Jomini_Lin <vince2641@gmail.com>
2017-01-03 22:45:53 +0800

    Add radial progress bar in the course information page.

--- public/javascripts/course_information.js
+++ public/javascripts/course_information.js
@@ -19,3 +19,34 @@ function openTab(evt, tabName) {
         document.getElementById(tabName).style.display = "block";
         evt.currentTarget.className += " active";
     }
+
+// 推薦度circle
+window.onload = function(maxScore, getScore) {
+    maxScore = 5; getScore = 3.1; // 課堂分數作為參數傳入或之後在此function中做讀該課程資料的動作
+    var i, radius = 100;
+    var circle = 2 * radius * Math.PI;
+    var elements = document.getElementsByTagName("circle");
+
+    // 設定circle大小，中心圓需要比較小
+    for(i = 0; i < elements.length; ++i) {
+        elements[i].setAttribute("r", radius);
+        elements[i].setAttribute("stroke-dasharray", circle);
+    }
+    elements = document.getElementsByClassName("radial-progress-center");
+    elements[0].setAttribute('r', (radius - 5));
+
+    var currentCount = 1;
+    var maxCount = 36; // 將一個圓分為幾個part.
+    var stopCount = getScore / maxScore * maxCount; // 總共轉幾個part.
+    var offset = 0;
+    elements = document.getElementsByClassName("radial-progress-cover");
+    var intervalId = setInterval(function() {
+        if(currentCount > stopCount) {
+            clearInterval(intervalId);
+            return;
+        }
+        offset = -(circle/maxCount) * currentCount;
+        elements[0].setAttribute("stroke-dashoffset", offset);
+        ++currentCount;
+    }, 30);
+}
